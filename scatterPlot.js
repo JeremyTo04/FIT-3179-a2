2dd54c2b2efb1069d80b1eae061a615d7d41fd9f
Jeremy To Jun Wei <111069787+JeremyTo04@users.noreply.github.com>
2023-10-11 13:52:55 +0800

    Add files via upload

---
+++ scatterPlot.js
@@ -0,0 +1,17 @@
+
+
+var scatterPlot = "scatterPlot.json";
+
+vegaEmbed("#scatter_plot", scatterPlot)
+  .then(function(result) {
+    // Access the Vega view instance as result.view
+    var view = result.view;
+    
+    // Now you can work with the Vega view
+    // For example, you can update data or interact with the visualization here
+    
+    // Example: Update data
+    var new_data = { /* your new data object */ };
+    view.change("source_0", vega.changeset().insert(new_data)).run();
+  })
+.catch(console.error);
